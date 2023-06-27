5a827228d441a64ffb45826fd6a44e8536ba2e83
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-06-27 17:19:03 +0000

    customize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./Pages/**/*.cshtml'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      currentColor: 'currentColor',
+      peach: {
+        light: '#FFAD9B',
+        DEFAULT: '#E7816B',
+        dark: '#E7816B',
+      },
+      grey: {
+        light: '#F1F3F5',
+        DEFAULT: '#333136',
+        dark: '#333136',
+      },
+      black: '#1D1C1E',
+      white: '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
