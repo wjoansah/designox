4b4136c813f53481d8456d2db9b44db823542f05
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 15:13:02 +0000

    add product showcase model class

---
+++ Models/ProductShowcase.cs
@@ -0,0 +1,9 @@
+﻿namespace DESIGNOX.Models
+{
+    public class ProductShowcase
+    {
+        public string Name { get; set; }
+        public string ImgPath { get; set; }
+        public string Description { get; set; }
+    }
+}
