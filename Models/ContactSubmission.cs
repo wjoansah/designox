6c572db6a4eecd6c6c274efa7811dd453eee6f93
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 14:59:46 +0000

    add contact sumbission model class

---
+++ Models/ContactSubmission.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DESIGNOX.Models
+{
+    public class ContactSubmission
+    {
+        [MinLength(3, ErrorMessage ="Name too short!")]
+        [Required(ErrorMessage ="Can't be empty!")]
+        public string Name { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "Can't be empty!")]
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        [MinLength(10, ErrorMessage ="Message too short!")]
+        public string Message { get; set; }
+    }
+}
