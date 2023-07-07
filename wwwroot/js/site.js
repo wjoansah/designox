52e01aabed8df9e4b92d7e2014404f7c403ed8e8
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 15:27:50 +0000

    add js to toggle navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let open = false
+const navToggle = document.getElementById('nav-toggle')
+const navMenu = document.getElementById('nav-menu')
+const menuIcon = document.getElementById('menu-icon')
+
+const stateIcons = {
+    opened: "/images/shared/mobile/icon-close.svg",
+    closed: "/images/shared/mobile/icon-hamburger.svg"
+}
+
+navToggle.addEventListener('click', () => {
+    open = !open
+    navMenu.classList.toggle('hidden')
+    menuIcon.src = open ? stateIcons.opened : stateIcons.closed
+})
