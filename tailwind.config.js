b6cec8e200b16bc43af450335e02b901bf933c96
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 15:13:55 +0000

    update tailwind peach colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Pages/**/*.cshtml'],
+  content: ["./Pages/**/*.cshtml"],
   theme: {
     colors: {
-      transparent: 'transparent',
-      currentColor: 'currentColor',
+      transparent: "transparent",
+      currentColor: "currentColor",
       peach: {
-        light: '#FFAD9B',
-        DEFAULT: '#E7816B',
-        dark: '#E7816B',
+        lighter: "#FDF3F0",
+        light: "#FFAD9B",
+        DEFAULT: "#E7816B",
+        dark: "#E7816B",
       },
       grey: {
-        light: '#F1F3F5',
-        DEFAULT: '#333136',
-        dark: '#333136',
+        light: "#F1F3F5",
+        DEFAULT: "#333136",
+        dark: "#333136",
       },
-      black: '#1D1C1E',
-      white: '#FFFFFF',
+      black: "#1D1C1E",
+      white: "#FFFFFF",
     },
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   plugins: [],
-}
+};
