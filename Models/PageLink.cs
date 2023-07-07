2124ea3891a559db5eec5c1a39bed9fbf0f10f62
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 15:12:44 +0000

    add page link model class

---
+++ Models/PageLink.cs
@@ -0,0 +1,9 @@
+﻿namespace DESIGNOX.Models
+{
+    public class PageLink
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+    }
+}
