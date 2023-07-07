89420ab6cad646c3c12a27383448dd2cef6578cc
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 15:47:13 +0000

    add locations page

---
+++ Pages/Locations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DESIGNOX.Pages
+{
+    public class LocationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
