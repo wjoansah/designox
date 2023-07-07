895be2c6d87bc1f684d84e78ffa5d8e14e080dc0
wjoansah <wonderful.owusu-ansah@amalitech.com>
2023-07-07 15:46:59 +0000

    add index page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DESIGNOX.Models;
+using DESIGNOX.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DESIGNOX.Pages
@@ -7,14 +9,20 @@ namespace DESIGNOX.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, LinkInfoService linkInfoService)
         {
             _logger = logger;
+            linkService = linkInfoService;
         }
+
+        public LinkInfoService linkService { get; set; }
+
+        [BindProperty]
+        public IEnumerable<PageLink> Links { get; set; } 
 
         public void OnGet()
         {
-
+            Links = linkService.GetPageLinks("home");
         }
     }
 }
